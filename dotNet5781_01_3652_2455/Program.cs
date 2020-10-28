b4f58ee369075264aaeb56ff00d1c458890eb4c5
k0532255001@gmail.com <k0532255001@gmail.com>
2020-10-28 14:14:19 +0200

    filed

--- dotNet5781_01_3652_2455/Program.cs
+++ dotNet5781_01_3652_2455/Program.cs
@@ -8,9 +8,13 @@ namespace dotNet5781_01_3652_2455
 {
     class Bus
     {
-        public int num;
-        public DateTime startTime;
-
+        public string LicensePlate;
+        public DateTime StartTime;
+        public DateTime DateOfTest;
+        public float TravelOfTest;
+        public float TotalTravel;
+        public float Fuel;
+        public 
     }
     class Program
     {
