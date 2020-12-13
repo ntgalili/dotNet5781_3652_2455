01bcff3fe87d1e9c1721455d242508324e3eb263
ntgalili <ntgalili@gmail.com>
2020-12-13 14:35:17 +0200

    *********************

--- dotNet5781_03B_3652_2455/AddBus.xaml.cs
+++ dotNet5781_03B_3652_2455/AddBus.xaml.cs
@@ -1,42 +1,42 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Shapes;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
-//namespace dotNet5781_03B_3652_2455
-//{
-//    /// <summary>
-//    /// Interaction logic for AddBus.xaml
-//    /// </summary>
-//    public partial class AddBus : Window
-//    {
-//        Bus newBus = new Bus();
-//        public AddBus()
-//        {
-//            InitializeComponent();
-//            grid1.DataContext = newBus;
-//        }
-//        private void Button_Click(object sender, RoutedEventArgs e)
-//        {
-//            try
-//            {
-//                BusesCollection.addBus(newBus);
-//                MessageBox.Show("succeeded", "perfect", MessageBoxButton.OK);
-//            }
-//            catch(Exception)
-//            {
-//                MessageBox.Show("dont succeeded, there is a bus with the same license plate", "please try again",MessageBoxButton.OK, MessageBoxImage.Error);
-//            }
-//            this.Close();
-//        }
-//    }
-//}
+namespace dotNet5781_03B_3652_2455
+{
+    /// <summary>
+    /// Interaction logic for AddBus.xaml
+    /// </summary>
+    public partial class AddBus : Window
+    {
+        Bus newBus = new Bus();
+        public AddBus()
+        {
+            InitializeComponent();
+            grid1.DataContext = newBus;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BusesCollection.addBus(newBus);
+                MessageBox.Show("succeeded", "perfect", MessageBoxButton.OK);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("dont succeeded, there is a bus with the same license plate", "please try again", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            this.Close();
+        }
+    }
+}
--- dotNet5781_03B_3652_2455/viewBusDetails.xaml.cs
+++ dotNet5781_03B_3652_2455/viewBusDetails.xaml.cs
@@ -1,102 +1,102 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Shapes;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
-//namespace dotNet5781_03B_3652_2455
-//{
-//    /// <summary>
-//    /// Interaction logic for viewBusDetails.xaml
-//    /// </summary>
-//    public partial class viewBusDetails : Window
-//    {
-//        Bus myBus;
-//        BackgroundWorker TestWorker;
-//        BackgroundWorker RefuelingWorker;
-//        public viewBusDetails(Bus B,bool  flag)
-//        {
-//            InitializeComponent();
-//            grid1.DataContext = B;
-//            myBus = B;
-//            grid1.IsEnabled = false;
-          
-
-//            TestWorker = new BackgroundWorker();
-//            TestWorker.DoWork += myBus.BusTest;
-//            TestWorker.ProgressChanged += TestWorker_ProgressChanged; ;
-//            TestWorker.RunWorkerCompleted += TestWorker_RunWorkerCompleted; ;
-//            TestWorker.WorkerReportsProgress = true;
-//            TestWorker.WorkerSupportsCancellation = true;
+namespace dotNet5781_03B_3652_2455
+{
+    /// <summary>
+    /// Interaction logic for viewBusDetails.xaml
+    /// </summary>
+    public partial class viewBusDetails : Window
+    {
+        Bus myBus;
+        BackgroundWorker TestWorker;
+        BackgroundWorker RefuelingWorker;
+        public viewBusDetails(Bus B, bool flag)
+        {
+            InitializeComponent();
+            grid1.DataContext = B;
+            myBus = B;
+            grid1.IsEnabled = false;
 
 
-//            RefuelingWorker = new BackgroundWorker();
-//            RefuelingWorker.DoWork += myBus.BusRefueling;
-//            RefuelingWorker.ProgressChanged += RefuelingWorker_ProgressChanged;
-//            RefuelingWorker.RunWorkerCompleted += RefuelingWorker_RunWorkerCompleted;
-//            RefuelingWorker.WorkerReportsProgress = true;
-//            RefuelingWorker.WorkerSupportsCancellation = true;
-//            if(myBus.BusStatus!=status.ready)
-//            {
-//                TestButton.IsEnabled = false;
-//                RefuelingButton.IsEnabled = false;
-//            }
-//            if (flag)
-//            {
-//                RefuelingWorker.RunWorkerAsync();
-//                TestButton.IsEnabled = false;
-//                RefuelingButton.IsEnabled = false;
-//            }
+            TestWorker = new BackgroundWorker();
+            TestWorker.DoWork += myBus.BusTest;
+            TestWorker.ProgressChanged += TestWorker_ProgressChanged; ;
+            TestWorker.RunWorkerCompleted += TestWorker_RunWorkerCompleted; ;
+            TestWorker.WorkerReportsProgress = true;
+            TestWorker.WorkerSupportsCancellation = true;
 
-//        }
 
-//        private void RefuelingWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-//        {
-//            LabelProgress.DataContext = "succseded";
-//            Thread.Sleep(2000);
-//            this.Close();
-//        }
+            RefuelingWorker = new BackgroundWorker();
+            RefuelingWorker.DoWork += myBus.BusRefueling;
+            RefuelingWorker.ProgressChanged += RefuelingWorker_ProgressChanged;
+            RefuelingWorker.RunWorkerCompleted += RefuelingWorker_RunWorkerCompleted;
+            RefuelingWorker.WorkerReportsProgress = true;
+            RefuelingWorker.WorkerSupportsCancellation = true;
+            if (myBus.BusStatus != status.ready)
+            {
+                TestButton.IsEnabled = false;
+                RefuelingButton.IsEnabled = false;
+            }
+            if (flag)
+            {
+                RefuelingWorker.RunWorkerAsync();
+                TestButton.IsEnabled = false;
+                RefuelingButton.IsEnabled = false;
+            }
 
-//        private void RefuelingWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
-//        {
-//            int progress = e.ProgressPercentage;
-//            LabelProgress.Content = (progress) + " Liters";
-//        }
+        }
 
-//        private void TestWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-//        {
-//            LabelProgress.DataContext = "succseded";
-//            Thread.Sleep(2000);
-//            this.Close();
-//        }
-//        private void TestWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
-//        {
-//            int progress = e.ProgressPercentage;
-//            LabelProgress.Content = "Tere are " +( 24-progress) + " hours left";
-//        }
+        private void RefuelingWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            LabelProgress.DataContext = "succseded";
+            Thread.Sleep(2000);
+            this.Close();
+        }
 
-//        private void TestButton_Click(object sender, RoutedEventArgs e)
-//        {
-//            TestWorker.RunWorkerAsync();
-//            TestButton.IsEnabled = false;
-//            RefuelingButton.IsEnabled = false;
-//        }
+        private void RefuelingWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int progress = e.ProgressPercentage;
+            LabelProgress.Content = (progress) + " Liters";
+        }
 
-//        private void RefuelingButton_Click(object sender, RoutedEventArgs e)
-//        {
-//            RefuelingWorker.RunWorkerAsync();
-//            TestButton.IsEnabled = false;
-//            RefuelingButton.IsEnabled = false;
-//        }
-//    }
-//}
+        private void TestWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            LabelProgress.DataContext = "succseded";
+            Thread.Sleep(2000);
+            this.Close();
+        }
+        private void TestWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int progress = e.ProgressPercentage;
+            LabelProgress.Content = "Tere are " + (24 - progress) + " hours left";
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            TestWorker.RunWorkerAsync();
+            TestButton.IsEnabled = false;
+            RefuelingButton.IsEnabled = false;
+        }
+
+        private void RefuelingButton_Click(object sender, RoutedEventArgs e)
+        {
+            RefuelingWorker.RunWorkerAsync();
+            TestButton.IsEnabled = false;
+            RefuelingButton.IsEnabled = false;
+        }
+    }
+}
