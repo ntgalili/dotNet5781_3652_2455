ed9d9308967a52001bccb3ac1161f8685919c172
ntgalili <ntgalili@gmail.com>
2020-12-13 14:22:13 +0200

    ///

--- dotNet5781_03B_3652_2455/MainWindow.xaml.cs
+++ dotNet5781_03B_3652_2455/MainWindow.xaml.cs
@@ -1,117 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
 
-namespace dotNet5781_03B_3652_2455
-{
-    public static class BusesCollection
-    {
-        internal static ObservableCollection<Bus> myBuses = new ObservableCollection<Bus>();
-        internal static void addBus(Bus myBus)
-        {
+//namespace dotNet5781_03B_3652_2455
+//{
+//    public static class BusesCollection
+//    {
+//        internal static ObservableCollection<Bus> myBuses = new ObservableCollection<Bus>();
+//        internal static void addBus(Bus myBus)
+//        {
 
-            foreach (Bus b in BusesCollection.myBuses)
-            {
-                if (b.LicensePlate == myBus.LicensePlate)
-                    throw new AddErrorException("There is a Bus With same License Plate");
-            }
-            BusesCollection.myBuses.Add(myBus);
-        }
-    }
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        static Random r = new Random();
+//            foreach (Bus b in BusesCollection.myBuses)
+//            {
+//                if (b.LicensePlate == myBus.LicensePlate)
+//                    throw new AddErrorException("There is a Bus With same License Plate");
+//            }
+//            BusesCollection.myBuses.Add(myBus);
+//        }
+//    }
+//    /// <summary>
+//    /// Interaction logic for MainWindow.xaml
+//    /// </summary>
+//    public partial class MainWindow : Window
+//    {
+//        static Random r = new Random();
 
-        /// <summary>
-        /// the method create a new random bus
-        /// </summary>
-        /// <returns>the new bus</returns>
-        Bus newBus()
-        {
-            DateTime date = new DateTime(r.Next(2000, 2020), r.Next(1, 12), r.Next(1, 28));//random date for when the bus ascended to the road
-            long num;
-            if(date.Year<2018)//the Bus number must match the year of the bus ascended to the road
-            {
-                num=r.Next(1000000, 9999999);
-            }
-            else
-            {
-               num= r.Next(10000000, 99999999);
-            }
-            return new Bus(num, date);
-        }
+//        /// <summary>
+//        /// the method create a new random bus
+//        /// </summary>
+//        /// <returns>the new bus</returns>
+//        Bus newBus()
+//        {
+//            DateTime date = new DateTime(r.Next(2000, 2020), r.Next(1, 12), r.Next(1, 28));//random date for when the bus ascended to the road
+//            long num;
+//            if(date.Year<2018)//the Bus number must match the year of the bus ascended to the road
+//            {
+//                num=r.Next(1000000, 9999999);
+//            }
+//            else
+//            {
+//               num= r.Next(10000000, 99999999);
+//            }
+//            return new Bus(num, date);
+//        }
 
-        void creatMyBuses()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                try
-                {
-                    BusesCollection.addBus(newBus());
+//        void creatMyBuses()
+//        {
+//            for (int i = 0; i < 10; i++)
+//            {
+//                try
+//                {
+//                    BusesCollection.addBus(newBus());
 
-                }
-                catch (Exception)
-                {
-                    i--;
-                }
-            }
-        }
+//                }
+//                catch (Exception)
+//                {
+//                    i--;
+//                }
+//            }
+//        }
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            creatMyBuses();
-            for(int i=0;i<3;i++)
-            {
-                BusesCollection.myBuses[r.Next(0, 10)].DateOfTest=DateTime.Now;
-                BusesCollection.myBuses[r.Next(0, 10)].Fuel=1200;
-                BusesCollection.myBuses[r.Next(0, 10)].TotalTravel+=20000;
-            }
-            ListOfBuses.DataContext = BusesCollection.myBuses;
-           // ListOfBuses.IsReadOnly = true;
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AddBus AddBusWindow = new AddBus();
-            AddBusWindow.ShowDialog();
-        }
+//        public MainWindow()
+//        {
+//            InitializeComponent();
+//            creatMyBuses();
+//            for(int i=0;i<3;i++)
+//            {
+//                BusesCollection.myBuses[r.Next(0, 10)].DateOfTest=DateTime.Now;
+//                BusesCollection.myBuses[r.Next(0, 10)].Fuel=1200;
+//                BusesCollection.myBuses[r.Next(0, 10)].TotalTravel+=20000;
+//            }
+//            ListOfBuses.DataContext = BusesCollection.myBuses;
+//           // ListOfBuses.IsReadOnly = true;
+//        }
+//        private void Button_Click(object sender, RoutedEventArgs e)
+//        {
+//            AddBus AddBusWindow = new AddBus();
+//            AddBusWindow.ShowDialog();
+//        }
 
-        private void Traveling_Click(object sender, RoutedEventArgs e)
-        {
-            Bus s = (sender as Button).DataContext as Bus;
-            MakeingTravel making = new MakeingTravel(s);
-            making.ShowDialog();
-        }
+//        private void Traveling_Click(object sender, RoutedEventArgs e)
+//        {
+//            Bus s = (sender as Button).DataContext as Bus;
+//            MakeingTravel making = new MakeingTravel(s);
+//            making.ShowDialog();
+//        }
 
-        private void ListOfBuses_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
+//        private void ListOfBuses_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+//        {
 
 
-            viewBusDetails myDetails = new viewBusDetails(((sender as ListBox).SelectedItem as Bus),false);
-            myDetails.Show();
+//            viewBusDetails myDetails = new viewBusDetails(((sender as ListBox).SelectedItem as Bus),false);
+//            myDetails.Show();
             
-        }
+//        }
 
-        private void Refueling_Click(object sender, RoutedEventArgs e)
-        {
-            Bus s = (sender as Button).DataContext as Bus;
-            viewBusDetails myDetails = new viewBusDetails(s,true);
-            myDetails.Show();
-        }
-    }
-}
+//        private void Refueling_Click(object sender, RoutedEventArgs e)
+//        {
+//            Bus s = (sender as Button).DataContext as Bus;
+//            viewBusDetails myDetails = new viewBusDetails(s,true);
+//            myDetails.Show();
+//        }
+//    }
+//}
--- dotNet5781_03B_3652_2455/MakeingTravel.xaml.cs
+++ dotNet5781_03B_3652_2455/MakeingTravel.xaml.cs
@@ -1,107 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Threading;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
+//using System.ComponentModel;
+//using System.Threading;
 
-namespace dotNet5781_03B_3652_2455
-{
-    /// <summary>
-    /// Interaction logic for MakeingTravel.xaml
-    /// </summary>
+//namespace dotNet5781_03B_3652_2455
+//{
+//    /// <summary>
+//    /// Interaction logic for MakeingTravel.xaml
+//    /// </summary>
 
-    public partial class MakeingTravel : Window
-    {
-        int kilometer;
-        Bus myBus;
-        BackgroundWorker travelWorker;
-        public MakeingTravel(Bus B)
-        {
-            InitializeComponent();
-            myBus = B;
-            grid1.DataContext = B;
-            grid1.IsEnabled = false;
-            travelWorker = new BackgroundWorker();
-            travelWorker.DoWork += travelWorker_DoWork;
-            travelWorker.ProgressChanged += travelWorker_ProgressChanged;
-            travelWorker.RunWorkerCompleted += travelWorker_RunWorkerCompleted;
-        }
-        private void travelWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            Random r = new Random();
-            int fast= r.Next(20, 51);
-            for(int i = 1; i < 7; i ++)
-            {
-                Thread.Sleep((kilometer / fast) * 1000);
-                travelWorker.ReportProgress(i);
-            }
-            e.Result = "the traveling passed successfully!";
-        }
-        private void travelWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            int progress = e.ProgressPercentage;
-            resultLabel.Content = (((kilometer/6.0)*progress) + " km /"+ kilometer + "km");
-        }
-        private void travelWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            myBus.BusStatus = (status)0;
-            MessageBox.Show(e.Result.ToString());
-            this.Close();
-        }
-        private void km_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            TextBox tx = sender as TextBox;
-            if (tx == null)return;
-            if (e == null)return;
+//    public partial class MakeingTravel : Window
+//    {
+//        int kilometer;
+//        Bus myBus;
+//        BackgroundWorker travelWorker;
+//        public MakeingTravel(Bus B)
+//        {
+//            InitializeComponent();
+//            myBus = B;
+//            grid1.DataContext = B;
+//            grid1.IsEnabled = false;
+//            travelWorker = new BackgroundWorker();
+//            travelWorker.DoWork += travelWorker_DoWork;
+//            travelWorker.ProgressChanged += travelWorker_ProgressChanged;
+//            travelWorker.RunWorkerCompleted += travelWorker_RunWorkerCompleted;
+//        }
+//        private void travelWorker_DoWork(object sender, DoWorkEventArgs e)
+//        {
+//            Random r = new Random();
+//            int fast= r.Next(20, 51);
+//            for(int i = 1; i < 7; i ++)
+//            {
+//                Thread.Sleep((kilometer / fast) * 1000);
+//                travelWorker.ReportProgress(i);
+//            }
+//            e.Result = "the traveling passed successfully!";
+//        }
+//        private void travelWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+//        {
+//            int progress = e.ProgressPercentage;
+//            resultLabel.Content = (((kilometer/6.0)*progress) + " km /"+ kilometer + "km");
+//        }
+//        private void travelWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+//        {
+//            myBus.BusStatus = (status)0;
+//            MessageBox.Show(e.Result.ToString());
+//            this.Close();
+//        }
+//        private void km_PreviewKeyDown(object sender, KeyEventArgs e)
+//        {
+//            TextBox tx = sender as TextBox;
+//            if (tx == null)return;
+//            if (e == null)return;
 
-            if (e.Key == Key.Enter || e.Key == Key.Return)
-            {
-                if (tx.Text.Length > 0)
-                {
-                    kilometer = int.Parse(tx.Text);
-                    tx.Text = "";
-                    bool flag = myBus.AddKM(kilometer);
-                    if (flag == true)
-                    {
-                        km.IsEnabled = false;
-                        myBus.BusStatus = (status)1;
-                        travelWorker.WorkerReportsProgress = true;
-                        travelWorker.RunWorkerAsync();
-                    }
-                    else
-                        MessageBox.Show("the bus can not make this travel");
-                }
-                e.Handled = true;
-                return;
-            }
-            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
-            if (char.IsControl(c)) return;
-            if (char.IsDigit(c)) return;
-            e.Handled = true;
-            MessageBox.Show("only numbers are allowed", "Account", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
+//            if (e.Key == Key.Enter || e.Key == Key.Return)
+//            {
+//                if (tx.Text.Length > 0)
+//                {
+//                    kilometer = int.Parse(tx.Text);
+//                    tx.Text = "";
+//                    bool flag = myBus.AddKM(kilometer);
+//                    if (flag == true)
+//                    {
+//                        km.IsEnabled = false;
+//                        myBus.BusStatus = (status)1;
+//                        travelWorker.WorkerReportsProgress = true;
+//                        travelWorker.RunWorkerAsync();
+//                    }
+//                    else
+//                        MessageBox.Show("the bus can not make this travel");
+//                }
+//                e.Handled = true;
+//                return;
+//            }
+//            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+//            if (char.IsControl(c)) return;
+//            if (char.IsDigit(c)) return;
+//            e.Handled = true;
+//            MessageBox.Show("only numbers are allowed", "Account", MessageBoxButton.OK, MessageBoxImage.Error);
+//        }
 
 
-        {
+//        {
 
-        }
+//        }
 
-        private void Window_Loaded_1(object sender, RoutedEventArgs e)
-        {
+//        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+//        {
 
-            System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // busViewSource.Source = [generic data source]
-        }
-    }
-}
+//            System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
+//            // Load data by setting the CollectionViewSource.Source property:
+//            // busViewSource.Source = [generic data source]
+//        }
+//    }
+//}
