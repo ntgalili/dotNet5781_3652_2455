a665f6569e10a5bf5570d1aa06073bd7cca59678
k0532255001@gmail.com <k0532255001@gmail.com>
2020-12-23 17:38:37 +0200

    Creating infrastructure for layers DO->Bus

---
+++ DLAPI/DO/Bus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class Bus
+    {
+        public int LicensePlate { get; set; }
+        public DateTime LicensingDate { get; set; }
+        public float TotalTrip { get; set; }
+        public float FuelRemain { get; set; }
+        public StatusOfBus Status { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return this.ToStringProperty();
+        //}
+
+        //private string ToStringProperty()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ DS/DataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DO;
+
+namespace DS
+{
+    public static class DataSource
+    {
+        public static List<Bus> ListOfBuses;
+        public static List<Line> ListOfLines;
+        public static List<LineStation> ListOfLineStation;
+        public static List<LineTrip> ListOfLineTrip;
+        public static List<Station> ListOfStation;
+        public static List<Trip> ListOfTrips;
+        public static List<User> ListOfUsers;
+        public static List<AdjacentStetions> ListOfAdjacentStation;
+    }
+}
