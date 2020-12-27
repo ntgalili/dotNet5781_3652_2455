3f7d0ed9aa8126c7442a4bc9024f11c836a4f0e4
ntgalili <ntgalili@gmail.com>
2020-12-27 23:19:33 +0200

    ds-InitAllLists

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -24,7 +24,7 @@ namespace DS
         static void InitAllLists()
         {
             ListBuses = new List<Bus>
-                    {
+            {
                         new Bus
                         {
                           LicensePlate= 12345678,
@@ -34,9 +34,7 @@ namespace DS
                           Status=StatusOfBus.Ready
                         },
 
-
-
-                          new Bus
+                        new Bus
                         {
                           LicensePlate= 23456789,
                           FromDate = new DateTime(2018/01/01),
@@ -45,7 +43,7 @@ namespace DS
                           Status=StatusOfBus.Ready
                         },
 
-                          new Bus
+                        new Bus
                         {
                           LicensePlate= 87654321,
                           FromDate = new DateTime(2018/01/01),
@@ -53,25 +51,95 @@ namespace DS
                           FuelRemain=1000,
                           Status=StatusOfBus.Ready
                         }
-                    };
+            };
+            
 
 
             ListLines = new List<Line>
-                    {
+            {
+                new Line
+                {
+                    LineNum=33,
+                    Code=1,
+                    Area=Areas.Jerusalem,
+                    FirstStation=1234,
+                    LastStation=9876
+                },
+                     
+                new Line
+                {
+                    LineNum=20,
+                    Code=5,
+                    Area=Areas.North,
+                    FirstStation=5555,
+                    LastStation=8493
+                },
 
-                        new Line
-                        {
-                           LineNum=14,
-                           Code=1,
-                           Area=Areas.Jerusalem,
-                           FirstStation=1234,
-                           LastStation=9876
-                        },
-                        //........
-                    };
+                new Line
+                {
+                    LineNum=79,
+                    Code=9,
+                    Area=Areas.General,
+                    FirstStation=5555,
+                    LastStation=9876
+                }
+            };
 
+            ListLineStations = new List<LineStation>
+            {
+                new LineStation
+                {
+                    LineNum=33,
+                    StationCode=1234,
+                    LineStationIndex=1,
+                    PrevStation=0,
+                    NextStation=9876
+                },
+                new LineStation
+                {
+                    LineNum=33,
+                    StationCode=9876,
+                    LineStationIndex=2,
+                    PrevStation=1234,
+                    NextStation=0
+                },
 
-            //......
+                new LineStation
+                {
+                    LineNum=20,
+                    StationCode=5555,
+                    LineStationIndex=1,
+                    PrevStation=0,
+                    NextStation=8493
+                },
+                new LineStation
+                {
+                    LineNum=33,
+                    StationCode=8493 ,
+                    LineStationIndex=2,
+                    PrevStation=5555,
+                    NextStation=0
+                },
+
+                new LineStation
+                {
+                    LineNum=79,
+                    StationCode=5555,
+                    LineStationIndex=1,
+                    PrevStation=0,
+                    NextStation=8493
+                },
+                new LineStation
+                {
+                    LineNum=33,
+                    StationCode=8493,
+                    LineStationIndex=2,
+                    PrevStation=5555,
+                    NextStation=0
+                },
+            };
+
+          
         }
     }
 }
