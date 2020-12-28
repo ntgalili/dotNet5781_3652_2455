57994a9846a7851a56928c2f3ffbe7e3ea4a35cf
k0532255001@gmail.com <k0532255001@gmail.com>
2020-12-28 11:02:06 +0200

    ...

--- BL/BO/Trip.cs
+++ BL/BO/Trip.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DO;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    class Trip : User,Station
+    class Trip : User
     {
         public int Id { get; set; }
         //public string UserName { get; set; }  -----------name of user
