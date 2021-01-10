b2a1414954e55a351f6b319f5e8f366738ffe521
ntgalili <ntgalili@gmail.com>
2021-01-10 22:39:04 +0200

    line stasion crud in IDL

--- DLAPI/DO/LineStation.cs
+++ DLAPI/DO/LineStation.cs
@@ -14,5 +14,45 @@ namespace DO
         public int PrevStation { get; set; }
         public int NextStation { get; set; }
 
+        public void AddStation(DO.LineStation ls)
+        {
+
+            DataSource.ListLineStations.Add(ls.Clone());
+        }
+
+        public void UpdateStation(DO.LineStation ls)
+        {
+            DO.LineStation toUpDate = DataSource.ListLineStations.Find(s => s.StationCode == ls.StationCode);
+            if (toUpDate != null)
+            {
+                DataSource.ListStations.Remove(toUpDate);
+                DataSource.ListStations.Add(ls.Clone());
+            }
+            else
+                throw new DO.BadStationCodeException(ls.StationCode, "Not found");
+        }
+        public void DeleteStation(int num)
+        {
+            DO.Station toDel;
+            toDel = DataSource.ListLineStations.FirstOrDefault(s => s.StationCode == num);
+            if (toDel == null)
+                throw new DO.BadStationCodeException(num, "Not found");
+            DataSource.ListLineStations.Remove(toDel);
+        }
+
+
+        public DO.LineStation GetStation(int num)
+        {
+            DO.Station toGet = DataSource.ListLineStations.Find(s => s.StationCode == num);
+            try
+            {
+                Thread.Sleep(2000); 
+            } 
+            catch (ThreadInterruptedException ex) { }
+            if (toGet != null)
+                return toGet.Clone();
+            else
+                throw new DO.BadStationCodeException(num, "Not found");
+        }
     }
 }
