ec5847c5925d4455b7902575e78f0cb0b2151fc1
k0532255001@gmail.com <k0532255001@gmail.com>
2021-02-04 19:28:39 +0200

    saveListToXML

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -35,15 +35,18 @@ namespace DS
 
 
 
-        public static void SaveListToXMLElement(XElement rootElem, string filePath)
+        public static void SaveListToXMLSerializer<T>(List<T> list, string filePath)
         {
             try
             {
-                rootElem.Save(@"xml\" + filePath);
+                FileStream file = new FileStream(dir + filePath, FileMode.Create);
+                XmlSerializer x = new XmlSerializer(list.GetType());
+                x.Serialize(file, list);
+                file.Close();
             }
             catch (Exception ex)
             {
-               /// throw new DO.XMLFileLoadCreateException(filePath, $"fail to create xml file: {filePath}", ex);
+                throw new DO.XMLFileLoadCreateException(filePath, $"fail to create xml file: {filePath}", ex);
             }
         }
 
